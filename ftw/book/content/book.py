85c2dbf4a975cbf0505cd96949bd5ea392560fbb
Julian Infanger <j.infanger@4teamwork.ch>
2009-08-06 08:53:33 +0000

    added fields

--- ftw/book/content/book.py
+++ ftw/book/content/book.py
@@ -13,7 +13,76 @@ from ftw.book.config import PROJECTNAME
 
 BookSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
 
-    # -*- Your Archetypes field definitions here ... -*-
+    atapi.BooleanField(
+	 	name = 'use_toc',
+	 	default = True,
+	    storage = atapi.AnnotationStorage(),
+	 	widget = atapi.BooleanWidget(
+	 		label = 'Show table of contents',
+		 	label_msgid = 'book_label_use_toc',
+		 	description = '',
+		 	description_msgid = 'book_help_use_toc',
+		 	i18n_domain = 'ftw.book',
+	 	),
+ 	),
+
+	atapi.BooleanField(
+	 	name = 'use_index',
+	 	default = True,
+	    storage = atapi.AnnotationStorage(),
+	 	widget = atapi.BooleanWidget(
+	 		label = 'Show index',
+		 	label_msgid = 'book_label_use_index',
+		 	description = '',
+		 	description_msgid = 'book_help_use_index',
+		 	i18n_domain = 'ftw.book',
+	 	),
+ 	),
+
+	atapi.BooleanField(
+	 	name = 'use_lot',
+	 	default = True,
+	    storage = atapi.AnnotationStorage(),
+	 	widget = atapi.BooleanWidget(
+	 		label = 'Show list of tables',
+		 	label_msgid = 'book_label_use_lot',
+		 	description = '',
+		 	description_msgid = 'book_help_use_lot',
+		 	i18n_domain = 'ftw.book',
+	 	),
+	),
+
+	atapi.BooleanField(
+	 	name = 'use_loi',
+	 	default = True,
+	    storage = atapi.AnnotationStorage(),
+	 	widget = atapi.BooleanWidget(
+	 		label = 'Show list of illustrations',
+		 	label_msgid = 'book_label_use_loi',
+		 	description = '',
+		 	description_msgid = 'book_help_use_loi',
+		 	i18n_domain = 'ftw.book',
+	 	),
+	),
+
+
+    atapi.StringField(
+            name = 'pagestyle',
+            default = 'oneside',
+		    storage = atapi.AnnotationStorage(),
+            vocabulary = (
+                    ('oneside', _(u'Einseitig',default=u'oneside')),
+                    ('twoside', _(u'Zweiseitig',default=u'twoside')),
+            ),
+            required = True,
+            widget = atapi.SelectionWidget(
+                    label = 'Pagestyle',
+                    label_msgid = 'book_label_pagestyle',
+                    description = '',
+                    description_msgid = 'book_help_pagestyle',
+				 	i18n_domain = 'ftw.book',
+            ),
+    ),
 
 ))
 
@@ -31,9 +100,16 @@ class Book(base.ATCTContent):
 
     meta_type = "Book"
     schema = BookSchema
+	
 
     title = atapi.ATFieldProperty('title')
     description = atapi.ATFieldProperty('description')
+    use_toc = atapi.ATFieldProperty('use_toc')
+    use_index = atapi.ATFieldProperty('use_index')
+    use_lot = atapi.ATFieldProperty('use_lot')
+    use_loi = atapi.ATFieldProperty('use_loi')
+    pagestyle = atapi.ATFieldProperty('pagestyle')
+	
     
     # -*- Your ATSchema to Python Property Bridges Here ... -*-
 
