69438b3c509f318d7334f237fa2627a85e50804b
Julian Infanger <j.infanger@4teamwork.ch>
2009-08-06 07:42:40 +0000

    created package with paster

---
+++ ftw/book/config.py
@@ -0,0 +1,8 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'ftw.book'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+}
---
+++ setup.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of ftw.book
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('ftw', 'book', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+    )
+
+tests_require=['zope.testing']
+
+setup(name='ftw.book',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      keywords='',
+      author='Julian Infanger',
+      author_email='info@4teamwork.ch',
+      url='https://svn.4teamwork.ch/repos/ftw/ftw.book/',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'ftw.book.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*- 
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
