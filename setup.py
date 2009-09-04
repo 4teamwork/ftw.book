f4ad78476d772caa97ddaea0d2f9d6c501d87345
Julian Infanger <j.infanger@4teamwork.ch>
2009-09-04 09:14:38 +0000

    moved version in version.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1'
+version = read('ftw','book','version.txt').strip()
 
 long_description = (
     read('README.txt')
