082e67150e6fe803d76a1ee7f294726c736cb807
Florian Sprenger <f.sprenger@4teamwork.ch>
2010-10-21 14:08:31 +0000

    fixed link latex

--- ftw/book/latex/link.py
+++ ftw/book/latex/link.py
@@ -6,7 +6,10 @@ class LinkLatexConverter(LatexCTConverter):
     def __call__(self, context, view):
         super(LinkLatexConverter, self).__call__(context, view)
         latex = []
-        latex.append(r'\href{%s}{%s}' % (
+	latex.append(view.convert(self.context.Title()))
+        latex.append(r'(\href{%s}{%s})' % (
             view.convert(self.context.remoteUrl),
-            view.convert(self.context.Title())))
+            view.convert(self.context.remoteUrl)))
+	latex.append(r'\newline')
+	latex.append(r'%s' % view.convert(self.context.getRawDescription()))
         return '\n'.join(latex)
