94a9659ce1bc370a543ee1c5e8166dbc64a408c5
Jonas Baumann <j.baumann@4teamwork.ch>
2011-08-09 11:10:45 +0000

    updated setup.py - simplyfied description

--- setup.py
+++ setup.py
@@ -5,31 +5,8 @@ This module contains the tool of ftw.book
 import os
 from setuptools import setup, find_packages
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = read('ftw','book','version.txt').strip()
+version = open('ftw/book/version.txt').read().strip()
 maintainer = 'Julian Infanger'
-
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('ftw', 'book', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
 
 tests_require=['zope.testing']
 
@@ -37,7 +14,9 @@ setup(name='ftw.book',
       version=version,
       description="" + \
           ' (Maintainer %s)' % maintainer,
-      long_description=long_description,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
@@ -55,6 +34,7 @@ setup(name='ftw.book',
       zip_safe=False,
       install_requires=['setuptools',
                         'plonegov.pdflatex',
+                        'simplelayout.base',
                         ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
