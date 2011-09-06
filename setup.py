54abd07bdfe20ead2c7abac5a20b81050254fd34
Jonas Baumann <j.baumann@4teamwork.ch>
2011-09-06 09:23:24 +0000

    cleaned up setup.py a little

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ from setuptools import setup, find_packages
 version = open('ftw/book/version.txt').read().strip()
 maintainer = 'Julian Infanger'
 
-tests_require=['zope.testing']
+tests_require = [
+    'zope.testing'
+    ]
 
 setup(name='ftw.book',
       version=version,
@@ -17,28 +19,32 @@ setup(name='ftw.book',
       long_description=open("README.txt").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
 
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='',
+
+      keywords='ftw book',
       author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='https://svn.4teamwork.ch/repos/ftw/ftw.book/',
+
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', ],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
+
+      install_requires=[
+        'setuptools',
         'simplelayout.types.common',
-                        'plonegov.pdflatex',
-                        'simplelayout.base',
-                        ],
+        'plonegov.pdflatex',
+        'simplelayout.base',
+        ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      test_suite = 'ftw.book.tests.test_docs.test_suite',
+      test_suite='ftw.book.tests.test_docs.test_suite',
       )
