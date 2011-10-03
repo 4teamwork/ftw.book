64f1d9801c8672fb20a30786a773ece5506b368d
Jonas Baumann <jone@jone.ch>
2011-10-03 16:37:09 +0200

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-This module contains the tool of ftw.book
-"""
 import os
 from setuptools import setup, find_packages
+
 
 version = open('ftw/book/version.txt').read().strip()
 maintainer = 'Julian Infanger'
 
+
 tests_require = [
     'zope.testing'
     ]
+
 
 setup(name='ftw.book',
       version=version,
@@ -29,7 +28,7 @@ setup(name='ftw.book',
       author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='https://svn.4teamwork.ch/repos/ftw/ftw.book/',
+      url='http://github.com/4teamwork',
 
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
@@ -40,12 +39,11 @@ setup(name='ftw.book',
       install_requires=[
         'setuptools',
         'simplelayout.types.common',
-        'plonegov.pdflatex',
         'simplelayout.base',
+        'plonegov.pdflatex',
         'archetypes.schemaextender',
         ],
 
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      test_suite='ftw.book.tests.test_docs.test_suite',
       )
