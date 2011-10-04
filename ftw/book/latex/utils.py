7da247fc91d00ec1a351442368d800ce8efd7e10
Jonas Baumann <jone@jone.ch>
2011-10-04 10:18:34 +0200

    cleanup latex utils

--- ftw/book/latex/utils.py
+++ ftw/book/latex/utils.py
@@ -1,7 +1,6 @@
+from ftw.book.interfaces import IBook
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
-from ftw.book.interfaces import IBook
-from simplelayout.types.common.interfaces import IPage
 
 HEADING_COMMANDS = [
     'chapter',
@@ -10,14 +9,18 @@ HEADING_COMMANDS = [
     'subsubsection',
     'paragraph',
     'subparagraph',
-]
+    ]
+
 
 def getLatexHeading(context, view, toc=True):
     title = view.convert(context.pretty_title_or_id())
+
     # level: depth of rendering
     level = view.level
+
     # rootObject: object, on which as_pdf was run
     rootObject = view.context
+
     # fix level depending of rootObject type
     bookOject = rootObject
     while not IBook.providedBy(bookOject):
@@ -25,19 +28,23 @@ def getLatexHeading(context, view, toc=True):
         level += 1
         if INavigationRoot.providedBy(bookOject):
             break
+
     # decrement level with 2 so that book is -1 and chapter is 0
     level -= 2
+
     # default command is last in HEADING_COMMANDS
     command = HEADING_COMMANDS[-1]
     if level<len(HEADING_COMMANDS):
         command = HEADING_COMMANDS[level]
+
     # generate latex
     tocmark = ''
     if not toc:
         tocmark = '*'
+
     latex = '\%s%s{%s}\n' % (
         command,
         tocmark,
-        title,
-    )
+        title)
+
     return latex
