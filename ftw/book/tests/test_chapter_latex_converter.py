9dc2b806a7e1c8a27815729ad3dadf75d4f198d0
Jonas Baumann <jone@jone.ch>
2011-10-04 15:18:45 +0200

    Add chapter LaTeX converter tests.

---
+++ ftw/book/tests/test_chapter_latex_converter.py
@@ -0,0 +1,37 @@
+from Acquisition import aq_inner, aq_parent
+from mocker import ANY
+from ftw.book.interfaces import IBook
+from ftw.book.latex.chapter import ChapterLatexConverter
+from plone.mocktestcase import MockTestCase
+from zope.interface import directlyProvides
+
+
+class TestChapterLatexConverter(MockTestCase):
+
+    def test_converter(self):
+        request = self.create_dummy()
+
+        book = self.create_dummy()
+        directlyProvides(book, IBook)
+
+        context = self.mocker.mock()
+        self.expect(aq_parent(aq_inner(context))).result(book)
+        self.expect(context.pretty_title_or_id()).result('chapter title')
+
+        view = self.mocker.mock()
+        self.expect(view.level).result(1)
+        self.expect(view.convert('chapter title')).result(
+            'converted chapter title')
+        self.expect(view.context).result(context)
+
+        patch = self.mocker.patch(ChapterLatexConverter)
+        self.expect(patch.convertChilds(ANY, ANY)).result(
+            'child LaTeX')
+
+        self.replay()
+
+        latex = ChapterLatexConverter(context, request)(context, view)
+
+        self.assertEqual(latex, '\n'.join((
+                    '\chapter{converted chapter title}',
+                    'child LaTeX')))
