d475d80cb79ddf44d4c551a8d7621a09f856bb02
Jonas Baumann <jone@jone.ch>
2011-10-04 16:35:54 +0200

    Add Latex injection test for brain support.

--- ftw/book/tests/test_latex_injection_rendering.py
+++ ftw/book/tests/test_latex_injection_rendering.py
@@ -50,3 +50,36 @@ class TestInjectionAwareConvertObject(MockTestCase):
         self.assertIn(latex_pre_code, latex)
         self.assertIn(latex_post_code, latex)
         self.assertIn('/'.join(obj.getPhysicalPath()), latex)
+
+    def test_injected_brain(self):
+        latex_pre_code = 'INJECTED PRE LATEX CODE'
+        latex_post_code = 'INJECTED POST LATEX CODE'
+
+        obj_dummy = self.create_dummy()
+
+        directlyProvides(obj_dummy, ILaTeXCodeInjectionEnabled)
+        obj = self.mocker.proxy(obj_dummy, spec=None)
+
+        self.expect(obj.getField('preLatexCode').get(obj)).result(
+            latex_pre_code)
+        self.expect(obj.getField('postLatexCode').get(obj)).result(
+            latex_post_code)
+
+        self.expect(obj.getPhysicalPath()).result(
+            ['', 'myobj']).count(0, None)
+        self.expect(obj.restrictedTraverse(
+                '/myobj/pdflatex_convert_object')).result(None)
+        self.expect(obj.restrictedTraverse('/myobj')).result(obj)
+
+        brain = self.mocker.mock()
+        self.expect(brain.getPath()).result('/myobj').count(2)
+
+        view = self.mocker.mock()
+        self.expect(view.context).result(obj).count(2)
+
+        self.replay()
+        latex = LatexCTConverter.convertObject(view, brain=brain)
+
+        self.assertIn(latex_pre_code, latex)
+        self.assertIn(latex_post_code, latex)
+        self.assertIn('/'.join(obj.getPhysicalPath()), latex)
