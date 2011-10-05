67601fe80dcdd55b04a8881813115f0575ae2399
Jonas Baumann <jone@jone.ch>
2011-10-05 12:20:02 +0200

    Cleanup: pep8

--- ftw/book/browser/nextprevious.py
+++ ftw/book/browser/nextprevious.py
@@ -1,19 +1,20 @@
-from zope.interface import implements
-from zope.component import adapts
-
-from plone.app.layout.nextprevious.interfaces import INextPreviousProvider
-from ftw.book.interfaces import IBook
 from Products.ATContentTypes.browser import nextprevious
+from ftw.book.interfaces import IBook
+from plone.app.layout.nextprevious.interfaces import INextPreviousProvider
+from zope.component import adapts
+from zope.interface import implements
+
 
 class BookNextPrevious(nextprevious.ATFolderNextPrevious):
-    """Let a folder act as a next/previous provider. This will be 
+    """Let a folder act as a next/previous provider. This will be
     automatically found by the @@plone_nextprevious_view and viewlet.
     """
 
     implements(INextPreviousProvider)
     adapts(IBook)
-    
+
     def buildNextPreviousQuery(self, *args, **kwargs):
-        query=super(BookNextPrevious,self).buildNextPreviousQuery(*args,**kwargs)
+        query = super(BookNextPrevious, self).buildNextPreviousQuery(
+            *args, **kwargs)
         del query['is_folderish']
         return query
