5c443c125d303a196dfafd662136f15bde03499c
Elio Schmutz <e.schmutz@4teamwork.ch>
2011-11-15 17:18:12 +0100

    override blockview from simplelayout

---
+++ ftw/book/browser/blockview.py
@@ -0,0 +1,10 @@
+from simplelayout.types.common.browser.views import BlockView
+from ftw.book.helpers import Numbering
+
+
+class BlockView(BlockView):
+
+    numbering = Numbering()
+
+    def get_dynamic_title(self):
+        return self.numbering(self)
