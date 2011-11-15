c98522b38378832f32f4d1b8ec1af0a1adaefcc6
Jonas Baumann <jone@jone.ch>
2011-11-15 16:00:42 +0100

    Add a IWithinBookLayer request layer.

--- ftw/book/interfaces.py
+++ ftw/book/interfaces.py
@@ -5,6 +5,12 @@ class IBook(Interface):
     """example book"""
 
 
+class IWithinBookLayer(Interface):
+    """Request layer interface, automatically provided by request
+    when traversing over book.
+    """
+
+
 class ILaTeXCodeInjectionEnabled(Interface):
     """Enables LaTeX code injection for admins on
     book-objects (chapters, SL-paragraphs).
