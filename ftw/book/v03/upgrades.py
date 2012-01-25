a4f7f56c71daf4963d373bf040e1ba369df244de
Jonas Baumann <jone@jone.ch>
2012-01-25 11:29:06 +0100

    Add upgrade step for removing old actions.

--- ftw/book/v03/upgrades.py
+++ ftw/book/v03/upgrades.py
@@ -1,5 +1,22 @@
 from Products.CMFCore.utils import getToolByName
 from ftw.book.content.chapter import Chapter
+from simplelayout.base.interfaces import ISimpleLayoutBlock
+from zope.interface import alsoProvides
+
+
+def remove_fti_action(setup, portal_type, action_id):
+    """Remove the action identified by ``action_id`` from
+    the FTI identified by ``portal_type``.
+    """
+    pt = getToolByName(setup, 'portal_types')
+    fti = pt[portal_type]
+
+    actions = []
+    for action in fti._actions:
+        if action.id != action_id:
+            actions.append(action)
+    fti._actions = tuple(actions)
+
 
 
 def migrate_chapter_classes(setup):
@@ -9,3 +26,12 @@ def migrate_chapter_classes(setup):
     for brain in brains:
         obj = brain.getObject()
         obj.__class__ = Chapter
+        alsoProvides(obj, ISimpleLayoutBlock)
+
+
+def remove_old_chapter_actions(setup):
+    remove_fti_action(setup, 'Chapter', 'edit-toggle')
+    remove_fti_action(setup, 'Chapter', 'metadata')
+    remove_fti_action(setup, 'Chapter', 'references')
+    remove_fti_action(setup, 'Chapter', 'preview')
+    remove_fti_action(setup, 'Chapter', 'history')
