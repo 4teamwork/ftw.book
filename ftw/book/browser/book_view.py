b82f98fa75cedefefd61a6cdd2c88bc3b4e1518d
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-03-29 17:22:56 +0200

    translate view names

--- ftw/book/browser/book_view.py
+++ ftw/book/browser/book_view.py
@@ -3,9 +3,11 @@ from plone.app.layout.navigation.navtree import buildFolderTree
 from ftw.book.browser.toc_tree import BookTocTree
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+
 class BookView(BrowserView):
 
     template = ViewPageTemplateFile('book_view.pt')
 
     def renderindex(self):
         return self.context.restrictedTraverse('@@index_view')()
+
