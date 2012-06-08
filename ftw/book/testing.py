c6557b88991a92f1fb474a7edc37bdb01337bf0d
Jonas Baumann <jone@jone.ch>
2012-06-08 16:00:09 +0200

    Add ZCML_LAYER testing layer.

--- ftw/book/testing.py
+++ ftw/book/testing.py
@@ -1,11 +1,12 @@
-from plone.testing import zca
-from plone.testing import Layer
+from ftw.testing.layer import ComponentRegistryLayer
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.app.testing import setRoles, TEST_USER_ID, TEST_USER_NAME, login
+from plone.testing import Layer
 from plone.testing import z2
+from plone.testing import zca
 from zope.configuration import xmlconfig
 
 
@@ -29,6 +30,19 @@ class LatexZCMLLayer(Layer):
 
 
 LATEX_ZCML_LAYER = LatexZCMLLayer()
+
+
+class ZCMLLayer(ComponentRegistryLayer):
+    """Test layer loading the complete package ZCML.
+    """
+
+    def setUp(self):
+        super(ZCMLLayer, self).setUp()
+        import ftw.book.tests
+        self.load_zcml_file('test.zcml', ftw.book.tests)
+
+
+ZCML_LAYER = ZCMLLayer()
 
 
 class FtwBookLayer(PloneSandboxLayer):
