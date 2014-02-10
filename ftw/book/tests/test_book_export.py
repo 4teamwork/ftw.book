5c831d6fcfb50e2c105b3fcd66d7fc8a355a74c3
Jonas Baumann <jone@jone.ch>
2014-02-10 15:04:41 +0100

    Add a bin/test-fast script, which does not run the full stack book export tests.

--- ftw/book/tests/test_book_export.py
+++ ftw/book/tests/test_book_export.py
@@ -1,9 +1,11 @@
 from ftw.book.testing import EXAMPLE_CONTENT_INTEGRATION_TESTING
 from ftw.book.tests.base import PDFDiffTestCase
+import os
 
 
-class TestPDFExport(PDFDiffTestCase):
+if not os.environ.get('SKIP_BOOK_EXPORTS', False):
+    class TestPDFExport(PDFDiffTestCase):
 
-    layer = EXAMPLE_CONTENT_INTEGRATION_TESTING
-    book_object_path = 'example-book'
-    expected_result = 'books/test_book_export.pdf'
+        layer = EXAMPLE_CONTENT_INTEGRATION_TESTING
+        book_object_path = 'example-book'
+        expected_result = 'books/test_book_export.pdf'
