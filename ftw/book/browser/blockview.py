ce2b89e3480718a5bfd2d03e4022fc972903791f
Jonas Baumann <jone@jone.ch>
2014-03-24 17:11:24 +0100

    Refactor Chapter to no longer base on simplelayout.types.common.

--- ftw/book/browser/blockview.py
+++ ftw/book/browser/blockview.py
@@ -1,7 +1,6 @@
 from Products.Five.browser import BrowserView
 from ftw.book.helpers import BookHelper
 from ftw.contentpage.browser.textblock_view import TextBlockView
-from simplelayout.types.common.browser.views import BlockView
 
 
 class BookTextBlockView(TextBlockView):
@@ -16,7 +15,7 @@ class HTMLBlockView(BrowserView):
         return BookHelper()(self.context)
 
 
-class BookChapterView(BlockView):
+class BookChapterView(BrowserView):
 
     def get_dynamic_title(self):
         return BookHelper()(self.context, linked=True)
