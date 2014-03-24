b8deba0204888096e798a539f74cbb6fc50d4615
Jonas Baumann <jone@jone.ch>
2014-03-24 15:45:28 +0100

    Remove no longer used BookBlockView.

--- ftw/book/browser/blockview.py
+++ ftw/book/browser/blockview.py
@@ -16,12 +16,6 @@ class HTMLBlockView(BrowserView):
         return BookHelper()(self.context)
 
 
-class BookBlockView(BlockView):
-
-    def get_dynamic_title(self):
-        return BookHelper()(self.context)
-
-
 class BookChapterView(BlockView):
 
     def get_dynamic_title(self):
