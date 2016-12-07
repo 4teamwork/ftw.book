062c45cd689c056ff9ca53b756838717e7e060d1
Jonas Baumann <jone@jone.ch>
2016-12-07 10:30:36 +0100

    text block FTI as dexterity.

--- ftw/book/browser/blockview.py
+++ ftw/book/browser/blockview.py
@@ -1,9 +1,23 @@
-from Products.Five.browser import BrowserView
+from ftw.book.contents2.textblock import IBookTextBlockSchema
 from ftw.book.helpers import BookHelper
-from ftw.contentpage.browser.textblock_view import TextBlockView
+from ftw.contentpage.browser import textblock_view
+from ftw.simplelayout.contenttypes.browser.textblock import TextBlockView
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class BookTextBlockView(TextBlockView):
+    template = ViewPageTemplateFile('templates/textblock.pt')
+    teaser_url = None
+
+    @property
+    def block_title(self):
+        if not IBookTextBlockSchema(self.context).show_title:
+            return ''
+        return BookHelper()(self.context)
+
+
+class BookTextBlockViewOLD(textblock_view.TextBlockView):
 
     def get_dynamic_title(self):
         return BookHelper()(self.context)
