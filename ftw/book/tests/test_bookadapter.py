13eef3527a5d0ed6d762ce9011c2ed899ef866f0
Jonas Baumann <jone@jone.ch>
2017-01-26 16:20:26 +0100

    refactor book adapter test.

--- ftw/book/tests/test_bookadapter.py
+++ ftw/book/tests/test_bookadapter.py
@@ -1,29 +1,16 @@
 from ftw.book.interfaces import IBook
-from ftw.book.testing import FTW_BOOK_INTEGRATION_TESTING
-from ftw.builder import Builder
-from ftw.builder import create
-from unittest2 import TestCase
+from ftw.book.tests import FunctionalTestCase
 
 
-class TestBookAdapterReturnsBook(TestCase):
-    layer = FTW_BOOK_INTEGRATION_TESTING
-
-    def setUp(self):
-        self.book = create(Builder('book'))
+class TestBookAdapterReturnsBook(FunctionalTestCase):
 
     def test_on_book(self):
-        self.assertEquals(self.book, IBook(self.book))
+        self.assertEquals(self.example_book, IBook(self.example_book))
 
     def test_on_chapter(self):
-        chapter = create(Builder('chapter').within(self.book))
-        self.assertEquals(self.book, IBook(chapter))
+        chapter = self.example_book.introduction
+        self.assertEquals(self.example_book, IBook(chapter))
 
     def test_on_text_block(self):
-        chapter = create(Builder('chapter').within(self.book))
-        block = create(Builder('book textblock').within(chapter))
-        self.assertEquals(self.book, IBook(block))
-
-    def test_on_file(self):
-        chapter = create(Builder('chapter').within(self.book))
-        block = create(Builder('file').within(chapter))
-        self.assertEquals(self.book, IBook(block))
+        block = self.example_book.introduction.get('management-summary')
+        self.assertEquals(self.example_book, IBook(block))
