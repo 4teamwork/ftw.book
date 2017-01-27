a3a3ea9d37135d9b81ff00713e52e0ec5dfed244
Jonas Baumann <jone@jone.ch>
2017-01-27 17:39:32 +0100

    refactor table tests.

--- ftw/book/tests/builders.py
+++ ftw/book/tests/builders.py
@@ -66,6 +66,40 @@ class HTMLBlockBuilder(DexterityBuilder):
 builder_registry.register('book htmlblock', HTMLBlockBuilder)
 
 
+class TableBuilder(DexterityBuilder):
+    portal_type = 'ftw.book.Table'
+
+    def with_table(self, table):
+        """Fills the table with data represented as list of lists.
+        """
+        self.table = table
+        return self
+
+    def with_dummy_table(self):
+        return self.with_table(zip(map(str, range(0, 10, 2)),
+                                   map(str, range(1, 10, 2))))
+
+    def after_create(self, obj):
+        self._update_table_data(obj)
+        super(TableBuilder, self).after_create(obj)
+
+    def _update_table_data(self, obj):
+        table = getattr(self, 'table', None)
+        if not table:
+            return
+
+        active_columns = len(table[0])
+        for index, column in enumerate(obj.column_properties):
+            column['active'] = index < active_columns
+
+        obj.data = [dict([('column_%i' % num, value)
+                          for num, value in enumerate(row)])
+                    for row in table]
+
+builder_registry.register('table', TableBuilder)
+
+
+
 # class BookBuilder(ArchetypesBuilder):
 
 #     portal_type = 'Book'
@@ -124,41 +158,3 @@ builder_registry.register('book htmlblock', HTMLBlockBuilder)
 #     portal_type = 'ListingBlock'
 
 # builder_registry.register('listingblock', ListingBlockBuilder)
-
-
-# class TableBuilder(ArchetypesBuilder):
-
-#     portal_type = 'Table'
-
-#     def __init__(self, *args, **kwargs):
-#         super(TableBuilder, self).__init__(*args, **kwargs)
-#         self.table = None
-
-#     def with_table(self, table):
-#         """Fills the table with data represented as list of lists.
-#         """
-#         self.table = table
-#         return self
-
-#     def with_dummy_table(self):
-#         return self.with_table(zip(map(str, range(0, 10, 2)),
-#                                    map(str, range(1, 10, 2))))
-
-#     def after_create(self, obj):
-#         self._update_table_data(obj, self.table)
-#         super(TableBuilder, self).after_create(obj)
-
-#     def _update_table_data(self, obj, table):
-#         if not table:
-#             return
-
-#         column_properties = obj.getColumnProperties()
-#         for column_index in range(len(table[0])):
-#             column_properties[column_index]['active'] = True
-
-#         data = map(lambda row: dict([('column_%i' % num, value)
-#                                      for num, value in enumerate(row)]),
-#                    table)
-#         obj.setData(data)
-
-# builder_registry.register('table', TableBuilder)
