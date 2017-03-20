5375a507485058ebd6fd7a7bd7ca2920dd790391
Jonas Baumann <jone@jone.ch>
2017-03-20 17:35:10 +0100

    rewrite internal hyperlinks test.

--- ftw/book/tests/test_latex_internal_hyperlinks.py
+++ ftw/book/tests/test_latex_internal_hyperlinks.py
@@ -1,72 +1,35 @@
-from ftw.book.testing import FTW_BOOK_INTEGRATION_TESTING
-from ftw.builder import Builder
-from ftw.builder import create
-from ftw.pdfgenerator.interfaces import ILaTeXView
-from ftw.pdfgenerator.interfaces import IPDFAssembler
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from ftw.book.tests import FunctionalTestCase
 from plone.uuid.interfaces import IUUID
-from unittest2 import TestCase
-from unittest2 import skip
-from zope.component import getMultiAdapter
 
 
-@skip('XXX UPDATE ME')
-class TestBookInternalHyperlinksLaTeX(TestCase):
-    layer = FTW_BOOK_INTEGRATION_TESTING
-
-    def setUp(self):
-        self.book = create(Builder('book')
-                           .titled('the book')
-                           .with_layout(IDefaultBookLayoutSelectionLayer))
+class TestBookInternalHyperlinksLaTeX(FunctionalTestCase):
 
     def test_book_internal_hyperlinks_have_internal_reference(self):
-        chapter = create(Builder('chapter')
-                         .within(self.book)
-                         .titled('the chapter'))
+        self.htmlblock.content = ('Link to {}!'.format(
+            self.html_link_to(self.textblock2)))
 
-        html = 'Link to ' + \
-            '<a class="internal-link"' + \
-            ' href="resolveuid/%s">' % IUUID(chapter) + \
-            'The Chapter</a>' + \
-            '!'
-        block = create(Builder('book textblock')
-                       .within(chapter)
-                       .having(text=html))
-
-        request = block.REQUEST
-        assembler = getMultiAdapter((self.book, request), IPDFAssembler)
-        latex_view = getMultiAdapter((block, request, assembler.get_layout()),
-                                     ILaTeXView)
-
-        self.assertIn(
-            r'\hyperref[path:/plone/the-book/the-chapter]{The Chapter'
-            r'\footnote{See page'
-            r' \pageref{path:/plone/the-book/the-chapter}}}',
-            latex_view.render())
+        self.assertEquals(
+            'Link to {}!\n'.format(self.latex_hyperref_to(self.textblock2)),
+            self.get_latex_view_for(self.htmlblock).render())
 
     def test_spaces_are_not_escaped(self):
-        # Escaping spaces in URLs with %20 in LaTeX is bad because % is a comment.
-        chapter = create(Builder('chapter')
-                         .within(self.book)
-                         .titled('the chapter'))
+        self.grant('Manager')
+        aq_parent(aq_inner(self.textblock2)).manage_renameObject(
+            self.textblock2.getId(), 'the textblock')
+        self.htmlblock.content = self.html_link_to(self.textblock2)
+        self.assertEquals(
+            self.latex_hyperref_to(self.textblock2).strip(),
+            self.get_latex_view_for(self.htmlblock).render().strip())
 
-        document = create(Builder('file')
-                          .within(chapter)
-                          .titled('The File')
-                          .with_id('the file'))
+    def html_link_to(self, obj):
+        return '<a class="internal-link" href="resolveuid/{}">{}</a>'.format(
+            IUUID(self.textblock2), self.textblock2.Title())
 
-        html = '<a class="internal-link" href="resolveuid/{0}">Link</a>'.format(
-            IUUID(document))
-        block = create(Builder('book textblock')
-                       .within(chapter)
-                       .having(text=html))
-
-        request = block.REQUEST
-        assembler = getMultiAdapter((self.book, request), IPDFAssembler)
-        latex_view = getMultiAdapter((block, request, assembler.get_layout()),
-                                     ILaTeXView)
-
-        self.assertEqual(
-            r'\hyperref[path:/plone/the-book/the-chapter/the file]{Link'
-            r'\footnote{See page'
-            r' \pageref{path:/plone/the-book/the-chapter/the file}}}',
-            latex_view.render().strip())
+    def latex_hyperref_to(self, obj):
+        convert = self.get_latex_layout(obj).get_converter().convert
+        return (r'\hyperref[path:%(path)s]{%(title)s\footnote{'
+                r'See page \pageref{path:%(path)s}}}') % {
+                    'title': convert(obj.Title()),
+                    'path': '/'.join(obj.getPhysicalPath())}
