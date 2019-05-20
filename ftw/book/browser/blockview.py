edc794f4a593416d76c23f4e6dc32873ebd0fd37
Jonas Baumann <jone@jone.ch>
2019-05-20 17:56:43 +0200

    Cleanup and standardize block view customizations.

--- ftw/book/browser/blockview.py
+++ ftw/book/browser/blockview.py
@@ -1,9 +1,21 @@
 from ftw.book.toc import TableOfContents
 from ftw.htmlblock.browser.htmlblock import HtmlBlockView
-from ftw.simplelayout.contenttypes.browser import filelistingblock
+from ftw.simplelayout.contenttypes.browser.filelistingblock import FileListingBlockView
+from ftw.simplelayout.contenttypes.browser.galleryblock import GalleryBlockView
 from ftw.simplelayout.contenttypes.browser.textblock import TextBlockView
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class BookBlockMixin:
+    book_template = ViewPageTemplateFile('templates/titled_block_view.pt')
+
+    def __call__(self):
+        return self.book_template()
+
+    @property
+    def block_title(self):
+        return TableOfContents().html_heading(self.context, linked=False)
 
 
 class BookChapterView(BrowserView):
@@ -13,25 +25,17 @@ class BookChapterView(BrowserView):
         return TableOfContents().html_heading(self.context, linked=True)
 
 
-class BookTextBlockView(TextBlockView):
-    template = ViewPageTemplateFile('templates/textblock.pt')
+class BookTextBlockView(BookBlockMixin, TextBlockView):
     teaser_url = None
 
-    @property
-    def block_title(self):
-        return TableOfContents().html_heading(self.context)
+
+class BookFileListingBlockView(BookBlockMixin, FileListingBlockView):
+    pass
 
 
-class HTMLBlockView(HtmlBlockView):
-
-    @property
-    def block_title(self):
-        return TableOfContents().html_heading(self.context)
+class BookGalleryBlockView(BookBlockMixin, GalleryBlockView):
+    pass
 
 
-class BookFileListingBlockView(filelistingblock.FileListingBlockView):
-    template = ViewPageTemplateFile('templates/listingblock.pt')
-
-    @property
-    def block_title(self):
-        return TableOfContents().html_heading(self.context)
+class HTMLBlockView(BookBlockMixin, HtmlBlockView):
+    pass
