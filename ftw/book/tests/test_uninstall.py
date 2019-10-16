4052a318256b058da7dfab29a0350f52b583c276
Jonas Baumann <jone@jone.ch>
2019-10-16 14:29:57 +0200

    Synchronize Generic Setup profiles and upgrade steps.

--- ftw/book/tests/test_uninstall.py
+++ ftw/book/tests/test_uninstall.py
@@ -1,16 +1,8 @@
 from ftw.testing.genericsetup import apply_generic_setup_layer
 from ftw.testing.genericsetup import GenericSetupUninstallMixin
-from unittest2 import skip
 from unittest2 import TestCase
 
 
-@skip('XXX UPDATE ME')
 @apply_generic_setup_layer
 class TestGenericSetupUninstall(TestCase, GenericSetupUninstallMixin):
     package = 'ftw.book'
-    additional_products = (
-        'simplelayout.base',
-        'simplelayout.ui.base',
-        'simplelayout.ui.dragndrop',
-        'ftw.contentpage',
-        )
