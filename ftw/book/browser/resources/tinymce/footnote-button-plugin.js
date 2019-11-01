855d06b1c1510b9f0e3207ee05e6c169cbe94784
Matthias Osswald <m@osswald.li>
2019-11-01 18:22:07 +0100

    Register and style tinymce stuff.

--- ftw/book/browser/resources/tinymce/footnote-button-plugin.js
+++ ftw/book/browser/resources/tinymce/footnote-button-plugin.js
@@ -1,47 +1,39 @@
 (function(tinymce) {
-tinymce.PluginManager.add('footnote', function(editor) {
-  editor.addButton('footnote', {
-    text: 'Footnote',
-    icon: false,
-    onclick: function() {
-      var selectionContent = editor.selection.getContent();
+  tinymce.PluginManager.add('footnote', function(editor) {
+    editor.addButton('footnote', {
+      text: 'Footnote',
+      icon: false,
+      onclick: function() {
+        var selectionContent = editor.selection.getContent();
 
-      var inputSchema = [
-        {
-          type: 'textbox', name: 'footnote', label: 'Footnote'
-        }
-      ];
+        var inputSchema = [
+          {
+            type: 'textbox', name: 'footnote', label: 'Footnote', multiline: 'true'
+          }
+        ];
 
-      var textInputSchema = {
-        type: 'textbox', name: 'text', label: 'Text'
-      };
-
-      if (!selectionContent) {
-        inputSchema.splice(1, 0, textInputSchema);
+        editor.windowManager.open({
+          title: 'Add Footnote',
+          body: inputSchema,
+          onsubmit: function(e) {
+            var text = selectionContent ? selectionContent : e.data.text;
+            var button = editor.dom.createFragment('\
+              <span class="footnote" data-footnote="' + e.data.footnote + '">' + text + '</span>\
+              ');
+              editor.selection.setNode(button);
+            }
+        });
       }
+    });
 
-      editor.windowManager.open({
-        title: 'Add Footnote',
-        body: inputSchema,
-        onsubmit: function(e) {
-          var text = selectionContent ? selectionContent : e.data.text;
-          var button = editor.dom.createFragment('\
-            <span class="footnote" data-footnote="' + e.data.footnote + '">' + text + '</span>\
-          ');
-          editor.selection.setNode(button);
-        }
-      });
-    }
+    return {
+      getMetadata: function () {
+        return  {
+          name: "Footnote Button Plugin",
+          author: "Busykoala"
+        };
+      }
+    };
   });
-
-  return {
-    getMetadata: function () {
-      return  {
-        name: "Footnote Button Plugin",
-        author: "Busykoala"
-      };
-    }
-  };
-});
 
 })(window.tinymce);
