9d1fc94005d58946a754578c61aa8c26553cec57
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2019-11-04 12:02:46 +0100

    Add ftw.file (2.x) as dependency for ftw.file DX integration.

---
+++ ftw/book/upgrades/20191104125404_integrate_ftw_file_2_x/upgrade.py
@@ -0,0 +1,9 @@
+from ftw.upgrade import UpgradeStep
+
+
+class IntegrateFtwFile2X(UpgradeStep):
+    """Integrate ftw.file 2.x.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
