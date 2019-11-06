85503ed3758c0835f2af108e0b1205a62efea0e9
Jonas Baumann <jone@jone.ch>
2019-11-06 13:50:48 +0100

    Fix the title translation of chapters. (#105)
    The label of the title field of chapters used the wrong translation
    domain. We are no using the ftw.book domain for this label.

--- ftw/book/contents/chapter.py
+++ ftw/book/contents/chapter.py
@@ -1,21 +1,18 @@
+from ftw.book import _
 from ftw.book.interfaces import IChapter
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.content import Container
 from plone.supermodel.model import Schema
-from zope.i18nmessageid import MessageFactory
 from zope.interface import implements
 from zope.interface import provider
 from zope.schema import TextLine
-
-
-DXMF = MessageFactory('plone.app.dexterity')
 
 
 @provider(IFormFieldProvider)
 class IChapterSchema(Schema):
 
     title = TextLine(
-        title=DXMF(u'label_title', default=u'Title'),
+        title=_(u'label_title', default=u'Title'),
         required=True,
     )
 
