217e7afe2a08fd0ce8b5e714d4dc23c8510d7b80
Tina Gerber <tina.gerber@4teamwork.ch>
2020-03-24 15:02:37 +0100

    Keywords must be editable.
    Add a new keyword if no keyword exists for the selected word, edit the existing one otherwise.

--- ftw/book/browser/resources/tinymce/keyword-button-plugin.js
+++ ftw/book/browser/resources/tinymce/keyword-button-plugin.js
@@ -8,11 +8,14 @@
       icon: 'keyword',
       image: '++resource++ftw.book-resources/tinymce/keyword_icon.png',
       onclick: function() {
+        var span = editor.dom.getParent(editor.selection.getNode(), 'span');
+        var within_keyword = span && span.getAttribute('class') && span.getAttribute('class').indexOf('keyword') > -1;
         var selectionContent = editor.selection.getContent();
+        var title = within_keyword ? span.getAttribute('title') : selectionContent;
 
         var inputSchema = [
           {
-            type: 'textbox', name: 'keyword', label: 'Keyword', value: selectionContent
+            type: 'textbox', name: 'keyword', label: 'Keyword', value: title
           }
         ];
 
@@ -21,11 +24,16 @@
           body: inputSchema,
           onsubmit: function(e) {
             var text = selectionContent ? selectionContent : e.data.text;
-            var button = editor.dom.createFragment('\
-              <span class="keyword" title="' + e.data.keyword + '">' + text + '</span>\
-              ');
+            if (within_keyword) {
+              span.title = e.data.keyword
+            }
+            else {
+              var button = editor.dom.createFragment('\
+                <span class="keyword" title="' + e.data.keyword + '">' + text + '</span>\
+                ');
               editor.selection.setNode(button);
             }
+          }
         });
       }
     });
